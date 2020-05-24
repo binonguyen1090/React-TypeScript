503b91e7f66fa6205275880605badc6dfb03d212
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-24 13:51:02 -0500

    show todo item with  complete button

--- typescript-app/src/App.tsx
+++ typescript-app/src/App.tsx
@@ -1,11 +1,32 @@
 import React, {Fragment, useState} from 'react';
 import './App.css';
+
 type FormElem = React.FormEvent<HTMLFormElement>
+interface Itodo{
+  text: string
+  complete: boolean
+}
+
 export default function App(): JSX.Element {
   const [value, setValue] = useState<string>('')
+  const [todos, setTodos] = useState<Itodo[]>([])
+
+  const addTodo = (text: string ) => {
+    const newTodos: Itodo[] = [...todos, {text, complete: false}]
+    setTodos(newTodos)
+  }
+
   const handleSubmit = (e: FormElem):void => {
     e.preventDefault()
+    addTodo(value)
     setValue('')
+
+  }
+
+  const completeTodo = (index: number):void => {
+    const newTodos: Itodo[] = [...todos ]
+    newTodos[index].complete = !newTodos[index].complete
+    setTodos(newTodos)
   }
 
   return (
@@ -15,6 +36,17 @@ export default function App(): JSX.Element {
         <input type='text' value={value} onChange={e => setValue(e.target.value)} required />
         <button type='submit' >Add Todo</button>
       </form>
+      <section>
+        {
+          todos.map((todo: Itodo, index: number) => (
+            <Fragment key={index}>
+              <div style={{textDecoration: todo.complete ? 'line-through': ''}}>{todo.text}</div>
+              <button type='button' onClick={() => completeTodo(index)} > {todo.complete ? 'Complete' : 'Incomplete' }</button>
+            </Fragment>
+
+          ))
+        }
+      </section>
     </Fragment>
   );
 }
